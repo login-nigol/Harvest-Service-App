3154c4f6b5936cc0aba9031c0c5fc3f4b14ea172
Vadim Antipov <vadim.antipov@gmx.de>
2024-10-27 14:43:00 +0100

    refreshed...

--- src/main/java/booking_service/BookingServiceApplication.java
+++ src/main/java/booking_service/BookingServiceApplication.java
@@ -9,5 +9,4 @@ public class BookingServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookingServiceApplication.class, args);
 	}
-
 }
--- src/main/resources/database/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (name, surname, email, password, role)
-VALUES
-    ('Вадим', 'Антипов', 'evaplus@mail.ru', '123', 'ROLE_USER');
