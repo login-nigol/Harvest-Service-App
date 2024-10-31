7281839778656dd08a489b5b5420fb91fb8e83bc
Vadim Antipov <vadim.antipov@gmx.de>
2024-10-31 18:46:23 +0100

    Base project is configured.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-## Система онлайн-бронирования билетов.
+# Rural fresh produce delivery service.
+## Сервис доставки свежей сельской продукции.
+
+### [Презентация проекта Harvest](https://drive.google.com/file/d/1cXLKKV83s5VxB78NOx3EKiLW1GxCNufL/view?usp=sharing)
--- src/main/java/ru/harvest_service/HarvestServiceApplication.java
+++ src/main/java/ru/harvest_service/HarvestServiceApplication.java
@@ -1,13 +1,13 @@
-package booking_service;
+package ru.harvest_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookingServiceApplication {
+public class HarvestServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookingServiceApplication.class, args);
+		SpringApplication.run(HarvestServiceApplication.class, args);
 	}
 
 }
